25d6234d52a37310a5db1371ec1a5569111f89f8
Markus Baumer <markus@baumer.dev>
2022-10-22 14:39:54 +0200

    Restructure build, docs, coverage dirs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 module.exports = {
-  coverageDirectory: "<rootDir>/build/coverage",
+  collectCoverageFrom: ["src/*.ts", "src/lib/*.ts"],
+  coverageDirectory: "<rootDir>/coverage",
   roots: ["<rootDir>/src"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
