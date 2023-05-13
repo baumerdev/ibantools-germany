5beb91e727c4454194a62550ea88f245ac79937c
Markus Baumer <markus@baumer.dev>
2023-05-13 16:40:08 +0200

    Update copyright years

--- src/__tests__/lib/methods/method12.spec.ts
+++ src/__tests__/lib/methods/method12.spec.ts
@@ -1,6 +1,6 @@
 /**
  * ibantools-germany
- * Copyright (C) 2022 Markus Baumer <markus@baumer.dev>
+ * Copyright (C) 2022-2023 Markus Baumer <markus@baumer.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
